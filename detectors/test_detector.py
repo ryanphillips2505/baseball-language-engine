a93aa549cdc8d1d8fae4e2c2af0cccb780dcb564
ryanphillips2505 <ryanphillips2505@gmail.com>
2026-06-12 21:22:05 -0500

    Delete detectors/test_detector.py

--- detectors/test_detector.py
+++
@@ -1,8 +0,0 @@
-from dataset.loader import load_pa_blocks
-from detectors.event_detector import detect_event_types
-
-blocks = load_pa_blocks("samples/gamechanger_pa_blocks.txt")
-
-for block in blocks[:10]:
-    print("=" * 50)
-    print(detect_event_types(block))
