8074168117c082d478d4b3da65b3dc85e8b8ac11
ryanphillips2505 <ryanphillips2505@gmail.com>
2026-06-14 09:20:01 -0500

    Update test_bip_classifier.py

--- tests/test_bip_classifier.py
+++ tests/test_bip_classifier.py
@@ -1,16 +1,34 @@
 from classifiers.bip_classifier import is_ball_in_play
+from models.baseball_event import BaseballEvent
+from models.types import EventType
 
 
-def test_ground_ball_is_bip():
-    assert is_ball_in_play("GB") is True
+def make_event(event_type: EventType) -> BaseballEvent:
+    return BaseballEvent(
+        primary_event=event_type,
+        secondary_events=[],
+    )
 
 
-def test_fly_ball_is_bip():
-    assert is_ball_in_play("FB") is True
+def test_hit_events_are_bip():
+    assert is_ball_in_play(make_event(EventType.SINGLE)) is True
+    assert is_ball_in_play(make_event(EventType.DOUBLE)) is True
+    assert is_ball_in_play(make_event(EventType.TRIPLE)) is True
+    assert is_ball_in_play(make_event(EventType.HOME_RUN)) is True
 
 
-def test_bunt_is_bip():
-    assert is_ball_in_play("BUNT") is True
+def test_out_events_are_bip():
+    assert is_ball_in_play(make_event(EventType.GROUND_OUT)) is True
+    assert is_ball_in_play(make_event(EventType.FLY_OUT)) is True
+    assert is_ball_in_play(make_event(EventType.LINE_OUT)) is True
+    assert is_ball_in_play(make_event(EventType.POP_OUT)) is True
+
+
+def test_non_bip_events_are_not_bip():
+    assert is_ball_in_play(make_event(EventType.WALK)) is False
+    assert is_ball_in_play(make_event(EventType.HIT_BY_PITCH)) is False
+    assert is_ball_in_play(make_event(EventType.STRIKEOUT_SWINGING)) is False
+    assert is_ball_in_play(make_event(EventType.STRIKEOUT_LOOKING)) is False
 
 
 def test_none_is_not_bip():
