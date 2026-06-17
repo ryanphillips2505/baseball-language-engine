9f8c7d3a7d1db1302a3d0b3ee9c392af9df5000f
ryanphillips2505 <ryanphillips2505@gmail.com>
2026-06-16 23:40:36 -0500

    Create test_real_gc_parity.py

---
+++ tests/test_real_gc_parity.py
@@ -0,0 +1,8 @@
+Wyatt Jones singles on a bunt to third baseman Thatcher Jack.
+
+Gentry Hoke strikes out swinging, Caleb Adams pitching,
+Wyatt Jones remains at 1st.
+
+Preston Klose grounds out to shortstop Ethan Vinson,
+Wyatt Jones caught stealing 2nd,
+second baseman Julio Alonso Gutierrez.
